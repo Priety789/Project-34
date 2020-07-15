30ea2350b0603038a94ab34f82b8ef95e5ac50e4
Priety789 <65825660+Priety789@users.noreply.github.com>
2020-07-15 09:52:28 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,42 +6,42 @@ var foodS, foodStock;
 
 function preload() {
 	//load images here
-    dogImg = loadImage("images/Dog.png");
-    happyDogImg = loadImage("images/happydog.png");
+	dogImg = loadImage("images/Dog.png");
+	happyDogImg = loadImage("images/happydog.png");
 }
 
 function setup() {
-    createCanvas(600, 600);
-    dog = createSprite(300, 400, 20, 20);
-    dog.addImage(dogImg);
-    database = firebase.database();
-    foodStock = database.ref('food');
-    foodStock.on("value", readStock);
+	createCanvas(600, 600);
+	dog = createSprite(300, 400, 20, 20);
+	dog.addImage(dogImg);
+	dog.scale = 0.4;
+	database = firebase.database();
+	foodStock.on("value", readStock);
 }
 
 
 function draw() {  
-    background(46, 139, 87);
+	background(46, 139, 87);
 
-    if (keyWentDown(UP_ARROW)) {
-        writeStock(foodS);
-        dog.addImage(happyDogImg);
-    }
+	if (keyWentDown(UP_ARROW)) {
+		writeStock(foodS);
+		dog.addImage(happyDogImg);
+	}
 
-    drawSprites();
-    //add styles here
-    textSize(20);
-    fill(0);
-    text(foodStock, 100, 50);
-    text("Press the up arrow to feed the dog!", 150, 100);
+	drawSprites();
+	//add styles here
+	textSize(20);
+	fill(0);
+	text(foodStock, 100, 50);
+	text("Press the up arrow to feed the dog!", 150, 100);
 }
 
 function readStock(data) {
-    foodS = data.val();
+	foodS = data.val();
 }
 
 function writeStock(x) {
-    database.ref('/').update({
-        food: x
-    })
+	database.ref('/').update({
+		food: x
+	})
 }
